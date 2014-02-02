887cc47bd82177c23da9b089b98703f9156c3b88
Rebecca Mills <beccamills86@gmail.com>
2014-02-01 18:13:44 -0800

    First commit.

---
+++ src/com/influenzaproject/servlet/InfluenzaStatebyDate.java
@@ -0,0 +1,32 @@
+package com.influenzaproject.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class InfluenzaStatebyDate
+ */
+@WebServlet("/state-by-date")
+public class InfluenzaStatebyDate extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public InfluenzaStatebyDate() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
---
+++ src/com/influenzaproject/servlet/InfluenzaStatebySubtype.java
@@ -0,0 +1,32 @@
+package com.influenzaproject.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class InfluenzaStatebySubtype
+ */
+@WebServlet("/state-by-subtype")
+public class InfluenzaStatebySubtype extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public InfluenzaStatebySubtype() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
