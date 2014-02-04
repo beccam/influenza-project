c44e7ec6a5893184302c1ac4d638a307b66936c4
Rebecca Mills <beccamills86@gmail.com>
2014-02-03 20:43:25 -0800

    Second change

--- src/com/influenzaproject/servlet/InfluenzaStatebyDate.java
+++ src/com/influenzaproject/servlet/InfluenzaStatebyDate.java
@@ -1,6 +1,8 @@
 package com.influenzaproject.servlet;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -27,6 +29,9 @@ public class InfluenzaStatebyDate extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		String state = request.getParameter("state");
+		PrintWriter out = response.getWriter();
+		out.write("State " + state);
 	}
 
 }
