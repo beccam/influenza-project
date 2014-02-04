8385f4280f4043a6df7f0e2eca9dd27a8115547d
Rebecca Mills <beccamills86@gmail.com>
2014-02-03 20:43:58 -0800

    Second change

--- src/com/influenzaproject/servlet/InfluenzaStrainbyYear.java
+++ src/com/influenzaproject/servlet/InfluenzaStrainbyYear.java
@@ -1,6 +1,8 @@
 package com.influenzaproject.servlet;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -27,6 +29,9 @@ public class InfluenzaStrainbyYear extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		String strainYear = request.getParameter("strain-year");
+		PrintWriter out = response.getWriter();
+		out.write("Strain year " + strainYear);
 	}
 
 }
