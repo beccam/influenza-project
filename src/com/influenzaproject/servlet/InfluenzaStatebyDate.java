dd4b540dd69b753ec4b49b35e9597055d4277f41
Rebecca Mills <beccamills86@gmail.com>
2014-02-05 13:28:21 -0800

    forth commit

--- src/com/influenzaproject/servlet/InfluenzaStatebyDate.java
+++ src/com/influenzaproject/servlet/InfluenzaStatebyDate.java
@@ -9,6 +9,11 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.datastax.driver.core.Cluster;
+import com.datastax.driver.core.ResultSet;
+import com.datastax.driver.core.Row;
+import com.datastax.driver.core.Session;
+
 /**
  * Servlet implementation class InfluenzaStatebyDate
  */
@@ -29,9 +34,30 @@ public class InfluenzaStatebyDate extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		Cluster cluster = Cluster.builder().addContactPoint("localhost").build();
+
+		Session session = cluster.connect();
+		
+		
 		String state = request.getParameter("state");
 		PrintWriter out = response.getWriter();
 		out.write("State " + state);
+		
+		String Date = request.getParameter("Date");
+		out.write("date " + Date);
+		
+        String queryText = "SELECT subtype,strain_id state FROM influenza.state_date WHERE state = '" + state + "'" + " AND date ='" + Date +"'";
+		
+		ResultSet results = session.execute(queryText);
+		
+		for (Row row : results) {		
+			  String subtype = row.getString("subtype");
+			  String location = row.getString("strain_id");
+			  out.println("<tr>");
+			  out.println("<td>" + subtype + "</td>");
+			  out.println("<td>" + strain_id + "</td>");
+			  out.println("</tr>");
+			}
 	}
 
 }
