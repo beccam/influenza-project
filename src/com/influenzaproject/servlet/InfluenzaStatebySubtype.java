58dc9eb2c0e793c0280fb801e6b0bf67ecbaffbf
Rebecca Mills <beccamills86@gmail.com>
2014-02-05 13:28:11 -0800

    forth commit

--- src/com/influenzaproject/servlet/InfluenzaStatebySubtype.java
+++ src/com/influenzaproject/servlet/InfluenzaStatebySubtype.java
@@ -9,6 +9,11 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.datastax.driver.core.Cluster;
+import com.datastax.driver.core.ResultSet;
+import com.datastax.driver.core.Row;
+import com.datastax.driver.core.Session;
+
 /**
  * Servlet implementation class InfluenzaStatebySubtype
  */
@@ -29,6 +34,10 @@ public class InfluenzaStatebySubtype extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		Cluster cluster = Cluster.builder().addContactPoint("localhost").build();
+
+		Session session = cluster.connect();
+		
 		String subtype = request.getParameter("Subtype");
 		PrintWriter out = response.getWriter();
 		out.write("Subtype " + subtype);
@@ -36,6 +45,19 @@ public class InfluenzaStatebySubtype extends HttpServlet {
 		if ("state" != null) {
 		String state = request.getParameter("state");
 		out.write("State " + state);}
+		
+		String queryText = "SELECT date, strain_id FROM influenza.state_subtype WHERE subtype = '" + subtype + "'" + " AND state ='" + state +"'";
+		
+		ResultSet results = session.execute(queryText);
+		
+		for (Row row : results) {		
+			  String date = row.getDate("date").toString();
+			  String strain_id = row.getString("strain_id");
+			  out.println("<tr>");
+			  out.println("<td>" + date + "</td>");
+			  out.println("<td>" + strain_id + "</td>");
+			  out.println("</tr>");
+			}
 	}
 
 }
